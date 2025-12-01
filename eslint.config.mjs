da9893e0a92345712a9c0406d8bd3737f05f33ae
Chinh Vo Wili <chinhvowili@gmail.com>
2025-12-01 16:40:10 +0700

    add build file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,8 +2,19 @@ import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
+import jest from "eslint-plugin-jest";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
+  { 
+    files: ["**/*.{js,mjs,cjs,jsx}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"], 
+    languageOptions: { 
+      globals: globals.browser,
+    },
+    rules: {
+      // Optional Jest rules    
+    },
+  },
+  pluginReact.configs.flat.recommended 
 ]);
