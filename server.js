4683d8516c60ed7f59664d83d54f793a3026a57d
Chinh Vo Wili <chinhvowili@gmail.com>
2025-12-02 09:45:40 +0700

    add build file

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+app.get('/healthcheck', async (req, res) => {
+  res.status(200).end();
+});
+
+app.get('/health', async (req, res) => {
+  res.status(200).end();
+});
+
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
